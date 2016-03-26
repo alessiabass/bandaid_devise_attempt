e83d47fd13691344b0664c7a79c36514652d3ed2
alessiabass <alessia.bass@yahoo.ie>
2016-03-26 20:20:00 +0000

    lockable, confirmable, logout timer, registration controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
   end
+  
+  def index
+    if user_signed_in?
+      redirect_to users_path
+    else
+      redorect_to new_user_session_path
+    end
+  end
 end

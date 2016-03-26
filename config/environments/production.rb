d750b50c002f9faf76e0fc846b037f64c6e14365
giuditta <alessia.bass@yahoo.ie>
2016-03-26 15:05:00 +0000

    adding changes to dev and prod environment with mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+        address: "smtp.gmail.com",
+        port: 587,
+        authentication: "plain",
+        enable_starttls_auto: true,
+        user_name: "alessia.student@gmail.com",
+        password: "Led!Zeppelin1973Ail"
+  
+    }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
